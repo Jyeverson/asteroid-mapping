35248b70cf0e2760dc17208c655372b4cf13f8e1
jyeverson <jyeversonsantos@gmail.com>
2024-09-10 01:15:49 -0300

    chore: set up dotenv and environment files

---
+++ src/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  nasaApiKey: 'NASA_API_KEY'
+};
---
+++ src/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  nasaApiKey: 'NASA_API_KEY'
+};
